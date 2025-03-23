9204f92b0245d9c15bbf986a36ee99743bf08015
Stacy <stacyxberlin@gmail.com>
2025-03-23 15:07:19 +0100

    complete reviews

--- src/js/08-reviews.js
+++ src/js/08-reviews.js
@@ -1,37 +1,48 @@
-import {markupReviewCard, makeReviewCard} from './render-functions.js'
+import {makeReviewCard} from './render-functions.js'
 import axios from 'axios';
 import Swiper from 'swiper'
+import iziToast from 'izitoast';
 
 
 
 const refs ={
   swiperWrapper: document.querySelector('.swiper-wrapper'),
   buttonNext:document.querySelector('.button-next-js'),
-  buttonPrev:document.querySelector('.button-prev-js')
+  buttonPrev:document.querySelector('.button-prev-js'),
+  buttons: document.querySelector('.buttons-wrapper'),
 }
 
 const renderReviewCards = async ()=> {
-  const response = await axios.get('https://portfolio-js.b.goit.study/api/reviews')
-  const data = response.data;
+  try{
+    const response = await axios.get('https://portfolio-js.b.goit.study/api/reviews')
+    const data = response.data;
 
-  refs.swiperWrapper.innerHTML = makeReviewCard(data);
-  initSwiper();
+    refs.swiperWrapper.innerHTML = makeReviewCard(data);
 
+    initSwiper();
+
+  }catch(error){
+    iziToast.error({message: error.message});
+    refs.swiperWrapper.innerHTML = 'NOT FOUND'
+    refs.buttons.classList.add('hidden')
+  }
 }
 
 renderReviewCards()
 
 const initSwiper = ()=>{
-  const swiper = new Swiper('.swiper', {slidesPerView: 1 });
+  const swiper = new Swiper('.swiper', {slidesPerView: 1, breakpoints: {1280: {slidesPerView: 2, spaceBetween: 32}}});
 
   refs.buttonNext.addEventListener('click', () => {
     swiper.slideNext();
 
     if(swiper.activeIndex === 1){
       refs.buttonPrev.classList.add('active')
+      refs.buttonPrev.classList.remove('disableHoverFocus');
     }
-    if(swiper.activeIndex === swiper.slides.length-1){
+    if(swiper.isEnd){
       refs.buttonNext.classList.remove('active')
+      refs.buttonNext.classList.add('disableHoverFocus');
     }
   });
 
@@ -40,9 +51,11 @@ const initSwiper = ()=>{
 
     if(swiper.activeIndex === 0){
       refs.buttonPrev.classList.remove('active')
+      refs.buttonPrev.classList.add('disableHoverFocus');
     }
-    if(swiper.activeIndex < swiper.slides.length-1){
+    if(!swiper.isEnd){
       refs.buttonNext.classList.add('active')
+      refs.buttonNext.classList.remove('disableHoverFocus')
     }
   });
 
