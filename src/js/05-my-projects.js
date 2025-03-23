334a12c7bf2c63f785703feaeca034b1123189c4
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2025-03-24 01:38:27 +0200

    Update 05-my-projects.js

--- src/js/05-my-projects.js
+++ src/js/05-my-projects.js
@@ -17,7 +17,7 @@ const createProjCardElem = ({
     <source type="image/webp" srcset="${desk1x} 1x, ${desk2x} 2x" media="(min-width: 1280px)" />
     <source type="image/webp" srcset="${tab1x} 1x, ${tab2x} 2x" media="(min-width: 768px)" />
     <source type="image/webp" srcset="${mob1x} 1x, ${mob2x} 2x" media="(min-width: 320px)" />
-    <img class="my-projects-img" src="${desk1x}" alt="${title}" loading="lazy"/>
+    <img class="my-projects-img" src="${desk1x}" alt="${title}" width="1008"/>
   </picture>
   <p class="my-projects-subtitle">${subtitle}</p>
   <div class="link-card-container">
