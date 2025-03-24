8eb34dd215440ab226f824160ecfe9f6b953b67e
BakelD <bakeld85@gmail.com>
2025-03-24 12:47:28 +0100

    Add header, hero and burger-modal into project

---
+++ src/js/01-header.js
@@ -0,0 +1,31 @@
+const refs = {
+  btnBurger: document.querySelector('.burger-menu-btn'),
+  btnClose: document.querySelector('.burger-menu-close-btn'),
+  burgerModal: document.querySelector('.burger-menu-modal'),
+  switcher: document.querySelector('.theme-checkbox'),
+};
+
+refs.btnBurger.addEventListener('click', () => {
+  refs.burgerModal.classList.add('is-open');
+
+  refs.btnClose.addEventListener('click', onModalClose);
+  // document.body.style.overflow = 'hidden';
+});
+
+const onModalClose = e => {
+  refs.burgerModal.classList.remove('is-open');
+
+  e.target.removeEventListaner('click', onModalClose);
+  // document.body.style.overflow = 'visible';
+};
+
+refs.switcher.addEventListener('change', e => {
+  if (e.target.checked) {
+    document.body.classList.add('theme-light');
+    document.body.classList.remove('theme-dark');
+    return;
+  }
+
+  document.body.classList.add('theme-dark');
+  document.body.classList.remove('theme-light');
+});
